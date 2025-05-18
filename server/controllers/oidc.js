3cd97b74ac97df3d818beabaabe1cc5798a9edf9
Jeff Tian <jeff.tian@outlook.com>
2025-05-18 20:18:18 +0800

    fix: use sub instead of userId

--- server/controllers/oidc.js
+++ server/controllers/oidc.js
@@ -97,7 +97,7 @@ const oidcSignInCallback = async (ctx) => {
 
         console.log('userResponse = ', userResponse.data)
         strapi.log.info(`userResponse = ${JSON.stringify(userResponse.data)}`)
-        const email = userResponse.data.email ?? ((userResponse.data.id ?? userResponse.data.userId) + '@email-miss.' + (userResponse.data.userSource ?? 'unknown') + '.com');
+        const email = userResponse.data.email ?? ((userResponse.data.id ?? userResponse.data.sub) + '@email-miss.' + (userResponse.data.userSource ?? 'unknown') + '.com');
 
         // whitelist check
         await whitelistService.checkWhitelistForEmail(email)
